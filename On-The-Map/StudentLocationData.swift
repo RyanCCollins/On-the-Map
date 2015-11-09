f533fa36bcabb9435b389ad4684b6783ca741c3f
techrapport <info@techrapport.com>
2015-11-09 13:12:36 -0500

    Commit before pod install

--- On-The-Map/StudentLocationData.swift
+++ On-The-Map/StudentLocationData.swift
@@ -10,31 +10,31 @@ import UIKit
 
 struct StudentLocationData {
     
-    struct Name {
-        let First: String?
-        let Last: String?
-    }
-    
-    struct Geo {
-        let Latitude: Double?
-        let Longitude: Double?
-        let Descriptor: String?
-    }
-    
-    struct Object {
-        let ID: String?
-        let Key: String?
-        let UpdateTime: String?
-    }
-    
-    init(studentLocationDictionary: [String : AnyObject]) {
-        /* Initialize data from studentLocationDictionary */
-        Name.First = studentLocationDictionary[ParseClient.JSONResponseKeys.Name.First] as String
-        Name.Last = studentLocationDictionary[ParseClient.JSONResponseKeys.Name.Last]
-        Geo.Latitude = studentLocationDictionary[ParseClient.JSONResponseKeys.Geo.Last]
-        Geo.Longitude = studentLocationDictionary[ParseClient.JSONResponseKeys.Geo.Longitude]
-        Object.ID = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.ID]
-        Object.Key = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.Key]
-        Object.UpdateTime = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.UpdateTime]
-    }
+//    struct Name {
+//        let First: String?
+//        let Last: String?
+//    }
+//    
+//    struct Geo {
+//        let Latitude: Double?
+//        let Longitude: Double?
+//        let Descriptor: String?
+//    }
+//    
+//    struct Object {
+//        let ID: String?
+//        let Key: String?
+//        let UpdateTime: String?
+//    }
+//    
+//    init(studentLocationDictionary: [String : AnyObject]) {
+//        /* Initialize data from studentLocationDictionary */
+//        Name.First = studentLocationDictionary[ParseClient.JSONResponseKeys.Name.First] as String
+//        Name.Last = studentLocationDictionary[ParseClient.JSONResponseKeys.Name.Last]
+//        Geo.Latitude = studentLocationDictionary[ParseClient.JSONResponseKeys.Geo.Last]
+//        Geo.Longitude = studentLocationDictionary[ParseClient.JSONResponseKeys.Geo.Longitude]
+//        Object.ID = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.ID]
+//        Object.Key = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.Key]
+//        Object.UpdateTime = studentLocationDictionary[ParseClient.JSONResponseKeys.Object.UpdateTime]
+//    }
 }
